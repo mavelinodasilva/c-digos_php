d6589f3888696a5150e130ad6883632c6935a2ca
Marcelo Avelino <44980408+mavelinodasilva@users.noreply.github.com>
2019-08-12 20:13:39 -0300

    Create ExibirDiasDaSemanaPorExtenso.php
    Exibir dias da semana por extenso

---
+++ ExibirDiasDaSemanaPorExtenso.php
@@ -0,0 +1,5 @@
+<?php
+# Exibir dias da semana por extenso
+$DiaSemana = array ( 'Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado');
+echo $DiaSemana[date('w')];
+?>
