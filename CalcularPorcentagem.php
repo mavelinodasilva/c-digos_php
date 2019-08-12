f82f04cc7a2a6d3388865a3a81f894cb2ab69ff8
Marcelo Avelino <44980408+mavelinodasilva@users.noreply.github.com>
2019-08-12 20:08:24 -0300

    Create CalcularPorcentagem.php
    Calculo simples de porcentagem

---
+++ CalcularPorcentagem.php
@@ -0,0 +1,7 @@
+<?php
+$valor = 29.99; # Valor Original
+$percentual = 60 / 100.0; # 8%
+$valor_final = $valor - ( $percentual * $valor );
+echo "O valor final do produto é: ".$valor_final;
+# O resultado será 230,00 
+?>
