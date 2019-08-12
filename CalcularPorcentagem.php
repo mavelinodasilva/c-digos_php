f731bac55810f791a6f296a3bfed9f189c1a43b3
Marcelo Avelino <44980408+mavelinodasilva@users.noreply.github.com>
2019-08-12 20:09:01 -0300

    Update CalcularPorcentagem.php

--- CalcularPorcentagem.php
+++ CalcularPorcentagem.php
@@ -1,7 +1,7 @@
 <?php
 $valor = 29.99; # Valor Original
 $percentual = 60 / 100.0; # 8%
-$valor_final = $valor - ( $percentual * $valor );
-echo "O valor final do produto é: ".$valor_final;
+$valor_final = $valor - ( $percentual * $valor ); # Conta
+echo "O valor final do produto é: ".$valor_final; # Exibir
 # O resultado será 230,00 
 ?>
